c42b01d364b7541424432edbaf86b8fa50714778
Federica Colapaoli <federica.colapaoli@coresecure.com>
2019-11-14 18:32:49 -0500

    some get requests

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const people = require('./users-data.js')
 
 const app = express()
 
@@ -13,8 +14,59 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use(function validateBearerToken(req, res, next) {
+     const authToken = req.get('Authorization')
+     const apiToken = process.env.API_TOKEN
+
+     console.log('validate bearer token middleware')
+     if (!authToken || authToken.split(' ')[1] !== apiToken) {
+          return res.status(401).json({ error: 'Unauthorized request' })
+     }   
+        // move to the next middleware
+     next()
+})
+
 app.get('/', (req, res) => {
-       res.send('Fede, Speranza e Carita')
+     res.send('Fede, Speranza e Carita')
+})
+app.get('/users', (req, res) => {     
+     res.json(people)
+})
+app.get('/country', (req, res) => {
+     const { search = ""} = req.query;
+     // do some validation
+  if (!search) {
+     // mark is required
+     return res
+       .status(400)
+       .send('Please provide a country');
+   }
+     
+     let results = people
+        .filter(person =>
+          person
+             .country
+             .toLowerCase()
+             .includes(search.toLowerCase()));
+     res.json(results)
+})
+app.get('/month', (req, res) => {
+     const { search = ""} = req.query;
+     // do some validation
+  if (!search) {
+     // mark is required
+     return res
+       .status(400)
+       .send('Please provide a month');
+   }
+     
+     let results = people
+        .filter(person =>
+          person
+             .country
+             .toLowerCase()
+             .includes(search.toLowerCase()));
+     res.json(results)
 })
 
 app.use(function errorHandler(error, req, res, next) {
---
+++ src/users-data.js
@@ -0,0 +1,122 @@
+module.exports = [
+    {
+        "id": "1",
+        "first_name": "Alex",
+        "last_name" : "Swartz", 
+        "user_name" : "Axax",
+        "email" : "sincere@april.biz",
+        "password" : "123q", 
+        "country" : "Austria", 
+        "month" : "jan"
+    },
+    {
+        "id": "2",
+        "first_name": "Antonia",
+        "last_name" : "Howell", 
+        "user_name" : "Antonette",
+        "email" : "shanna@melissa.tv",
+        "password" : "123q", 
+        "country" : "Spain", 
+        "month" : "feb"
+    },
+    {
+        "id": "3",
+        "first_name": "Clementine",
+        "last_name" : "Bauch", 
+        "user_name" : "Samantha",
+        "email" : "nathan@yesenia.net",
+        "password" : "123q", 
+        "country" : "Greece", 
+        "month" : "mar"
+    },
+    {
+        "id": "4",
+        "first_name": "Patricia",
+        "last_name" : "Lebsack", 
+        "user_name" : "Kariann",
+        "email" : "julianne.OConner@kory.org",
+        "password" : "123q", 
+        "country" : "France", 
+        "month" : "apr"
+    },
+    {
+        "id": "5",
+        "first_name": "Chelsey",
+        "last_name" : "Dietrich", 
+        "user_name" : "Kamren",
+        "email" : "lucio_Hettinger@annie.ca",
+        "password" : "123q", 
+        "country" : "Venezuela", 
+        "month" : "may"
+    },
+    {
+        "id": "6",
+        "first_name": "Kurtis",
+        "last_name" : "Weissnat", 
+        "user_name" : "Skiles",
+        "email" : "karley_Dach@jasper.info",
+        "password" : "123q", 
+        "country" : "Germany", 
+        "month" : "jun"
+    },
+    {
+        "id": "7",
+        "first_name": "Nicholas",
+        "last_name" : "Runolfsdottir", 
+        "user_name" : "Maxime",
+        "email" : "telly.Hoeger@billy.biz",
+        "password" : "123q", 
+        "country" : "Russia", 
+        "month" : "jul"
+    },
+    {
+        "id": "8",
+        "first_name": "Glenna",
+        "last_name" : "Reichert", 
+        "user_name" : "Delphine",
+        "email" : "sherwood@rosamond.me",
+        "password" : "123q", 
+        "country" : "Canada", 
+        "month" : "aug"
+    },
+    {
+        "id": "9",
+        "first_name": "Clementina",
+        "last_name" : "DuBuque", 
+        "user_name" : "Moriah",
+        "email" : "fail@fefiemail.it",
+        "password" : "123q", 
+        "country" : "Australia", 
+        "month" : "sep"
+    },
+    {
+        "id": "10",
+        "first_name": "Fede",
+        "last_name" : "Cola", 
+        "user_name" : "Fede2",
+        "email" : "fedecola@fefi.it",
+        "password" : "123q", 
+        "country" : "Italy", 
+        "month" : "dec"
+    },
+    {
+        "id": "11",
+        "first_name": "Akito",
+        "last_name" : "Hayama", 
+        "user_name" : "Eric",
+        "email" : "notwithsana@gpost.jp",
+        "password" : "123q", 
+        "country" : "Italy", 
+        "month" : "dec"
+    },
+    {
+        "id": "12",
+        "first_name": "Michael",
+        "last_name" : "Scott", 
+        "user_name" : "dunder",
+        "email" : "mifflina@gpost.ny",
+        "password" : "password", 
+        "country" : "China", 
+        "month" : "jan"
+    },
+]

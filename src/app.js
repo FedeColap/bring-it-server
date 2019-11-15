7b22d78fea27b07000e7900f991ad89aea2c40d7
Federica Colapaoli <federica.colapaoli@coresecure.com>
2019-11-14 19:10:40 -0500

    1 post (registration) request

--- src/app.js
+++ src/app.js
@@ -5,14 +5,18 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const people = require('./users-data.js')
+const uuid = require('uuid/v4');
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')? 'tiny': 'common';
 
 app.use(morgan(morganOption))
+app.use(express.json());
 app.use(helmet())
 app.use(cors())
+
+const users = [];
 
 app.use(function validateBearerToken(req, res, next) {
      const authToken = req.get('Authorization')
@@ -29,9 +33,69 @@ app.use(function validateBearerToken(req, res, next) {
 app.get('/', (req, res) => {
      res.send('Fede, Speranza e Carita')
 })
+
 app.get('/users', (req, res) => {     
-     res.json(people)
+     //THIS IS THE ARRAY AT LINE 19, IF YOU WANNA PLAY WITH THE PEOPLE ARRAY, YOU HAVE TO SWITCH NAME
+     res.json(users)
 })
+app.post('/users', (req, res) => {
+     const { first_name, last_name, user_name, email, password, confirm_password } = req.body;
+     console.log(req.body);
+     // validation code here
+     if (!first_name || !last_name) {
+          return res
+          .status(400)
+          .send('Please provide your complete name');
+     }
+     
+     if (!password) {
+          return res
+          .status(400)
+          .send('Password required');
+     }
+     if (password !== confirm_password) {
+          return res
+          .status(400)
+          .send('Password and confirmation must match');
+     }
+     
+     if (!email) {
+          return res
+          .status(400)
+          .send('Email required');
+     }
+     if (user_name.length < 6 || user_name.length > 20) {
+          return res
+            .status(400)
+            .send('Username must be between 6 and 20 characters');
+        }   
+     // password length
+     if (password.length < 8 || password.length > 36) {
+          return res
+            .status(400)
+            .send('Password must be between 8 and 36 characters');
+     }   
+     // password contains digit, using a regex here
+     if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+          return res
+            .status(400)
+            .send('Password must be contain at least one digit');
+     }
+     // at this point all validation passed
+     const id = uuid();
+     const newUser = {
+          id,
+          first_name,
+          last_name,
+          user_name,
+          email, 
+          password,
+     };
+
+     users.push(newUser);
+
+     res.send('All validation passed');
+});
 app.get('/country', (req, res) => {
      const { search = ""} = req.query;
      // do some validation
